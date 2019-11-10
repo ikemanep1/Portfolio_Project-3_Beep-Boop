132bbf14cc4a2a87bebc398ed5310ad67a0493fd
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2019-11-10 00:10:12 -0800

    add back end and front end logic to make the program return a number's range of integers when submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 // back end
+var output = [];
+
 function conversion(input) {
   var take = parseInt($("input#numberInput").val());
   for (let i=0; i <= take; i++) {
+    output.push(i);
     console.log(i)
+    console.log(i)
+    // if (take.i.includes(3)) {
+    //   output.push("error!");
+    // } else if (take.i.includes(2)) {
+    //   output.push("boop!");
+    // } else if (take.i.includes(1)) {
+    //   output.push("beep!");
+    // } else (i.includes(i))
+    //   output.push(i);
+    }
   }
-}
+
 
 
 // front end
@@ -13,7 +26,8 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $("input#numberInput").val();
     conversion(input);
-    $("#numberOutput").text(outputFinal);
+    $(".results").show();
+    $("#numberOutput").text(output);
   });
 
 });
