6f5bacda89e3750152f7ab9d231fb76b42a94a9e
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2019-11-09 23:31:34 -0800

    add some back end lines, fix html to make include a button and make js work properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 // back end
-
+function conversion(input) {
+  var take = parseInt($("input#numberInput").val());
+  for (let i=0; i <= take; i++) {
+    console.log(i)
+  }
+}
 
 
 // front end
 $(document).ready(function() {
-  $("input#numberInput").submit(function(event) {
+  $("#userInterface").submit(function(event) {
     event.preventDefault();
     var input = $("input#numberInput").val();
     conversion(input);
